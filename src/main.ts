9e75d72793b12106ea567e6558f30302d78b1fdf
AzureHearted <lxs249638467@gmail.com>
2025-11-28 22:26:33 +0800

    docs: 调整注释

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,7 @@ import App from "./App.vue";
 
 // import router from "./router";
 
-// Varlet组件库(配置)
-// import "@/plugin/varlet";
-
-//naive-ui组件库(相关)
+// ? naive-ui组件库配置
 import "@/plugin/naive-ui";
 
 // 创建 Vue 容器
@@ -22,9 +19,11 @@ fragment.appendChild(appContainer);
 // 创建 Vue 应用
 const app = createApp(App);
 
+// s 使用插件
 app.use(createPinia());
 // app.use(router);
 
+// f 初始化函数
 function init() {
 	lowerHighZIndex();
 	app.mount(appContainer); // 挂载app
