9db4ea894f9ed9d0850d1b220ae220e7b3a73a63
AzureHearted <lxs249638467@gmail.com>
2025-12-02 00:58:47 +0800

    refactor(CardStore/utils/get-cards): 重构获取卡片逻辑并优化代码格式
    对 `get-cards.ts` 文件中的代码进行了重构，主要优化如下：
    - 修正了 `handleRegionGetInfo` 函数中获取 DOM 的逻辑，确保返回结果始终是一个 `HTMLElement` 数组的第一个元素。
    - 优化了 `getMeta` 函数中的图片和视频元信息获取逻辑，确保代码更加简洁和高效。
    - 调整了 `getCard` 函数中的任务队列配置，简化了不必要的配置项。
    - 修正了 `getDOM` 函数的返回类型，确保在不同模式下返回结果更加一致和合理。
    - 优化了代码格式，调整了缩进和注释，使代码更易读。

--- src/utils/dom/get-dom.ts
+++ src/utils/dom/get-dom.ts
@@ -8,49 +8,51 @@ type optionsType = {
 export default function getDOM(
 	selector: keyof HTMLElementTagNameMap,
 	options?: Partial<optionsType>
-): HTMLElement[] | HTMLElement | null;
+): HTMLElement[];
 export default function getDOM(
 	selector: string,
 	options?: Partial<optionsType>
-): HTMLElement[] | HTMLElement | null;
+): HTMLElement[];
 export default function getDOM(
 	selector: keyof HTMLElementTagNameMap | string,
 	options?: Partial<optionsType>
-): HTMLElement[] | HTMLElement | null {
+) {
 	// 默认选项
 	const defaultOptions: optionsType = {
 		mode: "first",
 		regionDOM: document,
 	};
-	// s 合并选项
+
+	// 合并选项
 	const { mode, regionDOM } = { ...defaultOptions, ...options };
+
 	// s 选择器为空则返回空值
 	if (!selector) {
-		if (mode === "all") {
-			return [] as HTMLElement[];
-		} else {
-			return null;
-		}
+		return [];
 	}
+
 	// s 拆分选择器(拆分用逗号分隔的选择器,用于调整最后结果的顺序)
 	const selectors = selector.split(",");
+
 	// s 获取dom
-	const doms = selectors.reduce((result, currSelector) => {
-		const match = regionDOM.querySelectorAll(currSelector);
-		result.push(...match);
+	const doms = selectors.reduce<HTMLElement[]>((result, currSelector) => {
+		const matches = regionDOM.querySelectorAll(currSelector);
+		result.push(...(Array.from(matches) as Array<HTMLElement>));
 		return result;
-	}, [] as Element[]);
+	}, []);
+
 	// console.log("selector", selector, "doms", doms);
+
 	// 判断返回结果
 	switch (mode) {
 		case "all":
 			// 返回所有匹配的元素数组
-			return (doms.length ? Array.from(doms) : []) as HTMLElement[];
+			return doms.length > 0 ? Array.from(doms) : [];
 		case "first":
 			// 返回第一个匹配的元素
-			return doms.length ? (doms[0] as HTMLElement) : null;
+			return doms.length ? [doms[0]] : [];
 		case "last":
 			// 返回最后一个匹配的元素
-			return doms.length ? (doms[doms.length - 1] as HTMLElement) : null;
+			return doms.length ? [doms[doms.length - 1]] : [];
 	}
 }
